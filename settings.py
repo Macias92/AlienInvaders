828c0414606f4672cff20f7d3e26db68140ba8c5
Macias92 <maciejstys92@gmail.com>
2023-01-06 22:40:07 +0100

    add new feature - aliens can shoot now

--- settings.py
+++ settings.py
@@ -9,34 +9,41 @@ class Settings:
         self.screen_width = 1680
         self.screen_height = 1000
         self.bg = pygame.image.load('images/space_background.png')
-        self.ship_limit = 1
+        self.ship_limit = 2
 
         # Bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 60, 0)
         self.bullets_allowed = 10
+        self.alien_bullet_color = (255, 255, 0)
 
         # Alien settings
         self.fleet_drop_speed = 10.0
-        self.alien_points = 50
+        self.last_alien_shot = pygame.time.get_ticks()
 
         # Change game speed (difficulty level):
         self.speedup_scale = 1.2
         self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings which are changing during the game"""
         self.ship_speed = 3.5
         self.bullet_speed = 2.0
-        self.alien_speed = 0.75
-        self.fleet_direction = 0.75
+        self.alien_speed = 0.5
+        self.fleet_direction = 0.5
+        self.alien_points = 50
+        self.alien_bullet_speed = 1.0
+        self.alien_bullet_cd = 1000  # alien bullet cooldown in miliseconds
 
     def increase_speed(self):
         """Change game speed settings"""
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
         self.ship_speed *= self.speedup_scale
+        self.alien_bullet_speed *= self.speedup_scale
+        self.alien_bullet_cd /= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
